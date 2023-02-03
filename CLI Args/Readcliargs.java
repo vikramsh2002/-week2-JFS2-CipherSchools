c18e8f341117a9e3a52a3cf430cc912c040dc4ec
vikramsh2002 <vikramsh2002@gmail.com>
2023-02-03 07:49:21 +0530

    Added CLI Args code

---
+++ CLI Args/Readcliargs.java
@@ -0,0 +1,17 @@
+class Student{
+
+    public static void main(String []args){
+
+    int n=args.length;
+     // reading values
+     System.out.println("List of Passed Students are : ");
+     for(int i=0;i<n;++i)
+     {
+     
+       System.out.println(""+args[i]);
+
+      }
+
+    }
+
+}
